79c3da3e4f72405f2104c2019561c99a79534e0e
Magnus Vodrup <magnus@visito.no>
2024-10-18 12:33:30 +0200

    cant read

--- src/main/kotlin/no/ks/svarut/klient/forsendelse/send/v2/builders/UtskriftskonfigurasjonBuilder.kt
+++ src/main/kotlin/no/ks/svarut/klient/forsendelse/send/v2/builders/UtskriftskonfigurasjonBuilder.kt
@@ -3,7 +3,7 @@ package no.ks.svarut.klient.forsendelse.send.v2.builders
 import no.ks.fiks.svarut.forsendelse.send.model.v2.Utskriftskonfigurasjon
 
 class UtskriftskonfigurasjonBuilder {
-    private var tosidig: Boolean? = null
+    private var tosidig: Boolean? = false
     private var utskriftMedFarger: Boolean? = false
 
     fun tosidig(tosidig: Boolean?) = apply { this.tosidig = tosidig }
