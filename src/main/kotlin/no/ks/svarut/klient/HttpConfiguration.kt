3fb70ffc85927c119e372445fd0a94959dee5313
Sondre Fjeldstad <sondre.fjeldstad@ks.no>
2025-01-09 14:40:47 +0100

    builder

--- src/main/kotlin/no/ks/svarut/klient/HttpConfiguration.kt
+++ src/main/kotlin/no/ks/svarut/klient/HttpConfiguration.kt
@@ -2,6 +2,16 @@ package no.ks.svarut.klient
 
 import java.time.Duration
 
-object HttpConfiguration {
-   var idleTimeout: Duration? = null
+data class HttpConfiguration(val idleTimeout: Duration?) {
+    companion object {
+        fun builder() = Builder()
+    }
+
+    class Builder {
+        private var idleTimeout: Duration? = null
+
+        fun idleTimeout(idleTimeout: Duration?) = apply { this.idleTimeout = idleTimeout }
+
+        fun build() = HttpConfiguration(idleTimeout)
+    }
 }
