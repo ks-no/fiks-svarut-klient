6e1430f1a9547aaf413db49932b26149973920ea
Sondre Fjeldstad <sondre.fjeldstad@ks.no>
2025-01-10 08:19:21 +0100

    refactoring

--- src/main/kotlin/no/ks/svarut/klient/HttpConfiguration.kt
+++ src/main/kotlin/no/ks/svarut/klient/HttpConfiguration.kt
@@ -10,7 +10,7 @@ data class HttpConfiguration(val idleTimeout: Duration? = null) {
     class Builder {
         private var idleTimeout: Duration? = null
 
-        fun idleTimeout(idleTimeout: Duration?) = apply { this.idleTimeout = idleTimeout }
+        fun idleTimeout(idleTimeout: Duration) = apply { this.idleTimeout = idleTimeout }
 
         fun build() = HttpConfiguration(idleTimeout)
     }
