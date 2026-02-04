beda3d685bc08f9fdd206ded57e213737af33e55
Andreas Hammerbeck <Andreas_93@hotmail.com>
2026-02-04 07:54:08 +0100

    KSSU-1240: Giroark skal være nullable for V3 dokument builders

--- src/main/kotlin/no/ks/svarut/klient/forsendelse/send/v3/builders/PersonDokumentBuilder.kt
+++ src/main/kotlin/no/ks/svarut/klient/forsendelse/send/v3/builders/PersonDokumentBuilder.kt
@@ -13,7 +13,7 @@ class PersonDokumentBuilder {
     fun filnavn(filnavn: String) = apply { this.filnavn = filnavn }
     fun mimeType(mimeType: String) = apply { this.mimeType = mimeType }
     fun skalSigneres(skalSigneres: Boolean?) = apply { this.skalSigneres = skalSigneres }
-    fun giroarkSide(giroarkSide: Int) = apply { if(giroarkSide < 1) throw IllegalArgumentException("Giroarkside må vere et heltall større enn 0") else this.giroarkSide = giroarkSide }
+    fun giroarkSide(giroarkSide: Int?) = apply { if (giroarkSide != null && giroarkSide < 1) throw IllegalArgumentException("Giroarkside må vere et heltall større enn 0") else this.giroarkSide = giroarkSide }
     fun skalEkskluderesFraUtskrift(skalEkskluderesFraUtskrift: Boolean?) = apply { this.skalEkskluderesFraUtskrift = skalEkskluderesFraUtskrift }
 
     fun build() = PersonForsendelseDokumenterInner(
